e3429e0ba1088f376ca1f6d43cb9e1bf57613704
akbarulazis <akbarulazis13@gmail.com>
2025-05-25 14:54:26 +0700

    uppdate settings

--- core/asgi.py
+++ core/asgi.py
@@ -1,10 +1,11 @@
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from .urls import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
